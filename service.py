9b0c18473e675f7bbc38c61f2be4510d74c77fdc
SIHYEON KIM <79949284+tlgus626@users.noreply.github.com>
2023-06-13 07:21:55 +0000

    upload train and service

--- service.py
+++ service.py
@@ -5,24 +5,23 @@ import pandas as pd
 from bentoml.io import JSON, NumpyNdarray
 from pydantic import BaseModel
 
-
+# X : datetime,season,holiday,workingday,weather,temp,atemp,humidity,windspeed
 class Features(BaseModel):
-    bhk: int
-    size: int
-    floor: str
-    area_type: str
-    city: str
-    furnishing_status: str
-    tenant_preferred: str
-    bathroom: int
-    point_of_contact: str
+    time: str
+    season: str
+    holiday: str
+    workingday: str
+    weather: str
+    temp: int
+    atemp: int
+    humidity: int
+    windspeed: int
 
 
 # TODO: 학습 코드에서 저장한 베스트 모델을 가져올 것 (house_rent:latest)
-bento_model = bentoml.sklearn.get("house_rent:latest")
+bento_model = bentoml.sklearn.get("bike_sharing:latest")
 model_runner = bento_model.to_runner()
-svc = bentoml.Service("rent_house_regressor", runners=[model_runner])
-# TODO: "rent_house_regressor"라는 이름으로 서비스를 띄우기
+svc = bentoml.Service("bike_sharing_regressor", runners=[model_runner])
 
 
 # Features 클래스를 JSON으로 받아오고 Numpy NDArray를 반환하도록 데코레이터 작성
